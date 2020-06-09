d719bc4ff2a612030073c934aaa4100f1826b45f
zhuyihui <zhuyihui@protonmail.com>
2020-06-09 19:01:02 +0200

    Updates

---
+++ precache-manifest.0b3c912e8e6626cb1cf8213a96e96967.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c63df0fe4644f8c0cefbdbc0b1b35c3",
+    "url": "/todo-webapp/index.html"
+  },
+  {
+    "revision": "f5c4bf2fb62688ba95ec",
+    "url": "/todo-webapp/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "f23ac944f1a0b5b6e3b3",
+    "url": "/todo-webapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f5c4bf2fb62688ba95ec",
+    "url": "/todo-webapp/static/js/2.cddc045f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo-webapp/static/js/2.cddc045f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23ac944f1a0b5b6e3b3",
+    "url": "/todo-webapp/static/js/main.8ff543fa.chunk.js"
+  },
+  {
+    "revision": "c6ecbc9a8a3207916848",
+    "url": "/todo-webapp/static/js/runtime-main.a77fc5f3.js"
+  },
+  {
+    "revision": "99d8216c3ea32783f846e6acacba4cd0",
+    "url": "/todo-webapp/static/media/Doubs.99d8216c.png"
+  },
+  {
+    "revision": "f323cf2d37f5b91eda063d1a0c485fcd",
+    "url": "/todo-webapp/static/media/Mataura.f323cf2d.png"
+  },
+  {
+    "revision": "73488154491c4d663faf65236268b6a7",
+    "url": "/todo-webapp/static/media/Vorderrhein.73488154.png"
+  },
+  {
+    "revision": "16e7b69d4a98edcd012e6f3456e9667e",
+    "url": "/todo-webapp/static/media/Waihou.16e7b69d.png"
+  }
+]);
